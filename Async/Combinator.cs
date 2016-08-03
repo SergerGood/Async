6a6dd2e57e8cfc3bff9061cd84e9678ad17b148c
SergerGood <serzher@inbox.ru>
2016-08-03 21:30:52 +0300

    Combinator

---
+++ Async/Combinator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Async
+{
+    public static class Combinator
+    {
+        private static async Task<T> WithTimeOut<T>(Task<T> task, int time)
+        {
+            Task delayTask = Task.Delay(time);
+            Task firstToFinish = await Task.WhenAny(task, delayTask);
+
+            if (firstToFinish == delayTask)
+            {
+                await task.ContinueWith(HandleException);
+                throw new TimeoutException();
+            }
+
+            return await task;
+        }
+
+        private static void HandleException<T>(Task<T> task)
+        {
+            if (task.Exception != null)
+            {
+                Console.WriteLine(task.Exception);
+            }
+        }
+    }
+}
